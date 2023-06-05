e511e6125fb42dc2171d2a19cea762608ac1e0a8
lbwei <0800680274united@gmail.com>
2023-06-05 22:33:33 +0800

    merge_mask

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import segmentation
 import inpainting
 import os
-import numpy as np
-import PIL
+# import numpy as np
+from PIL import Image
+from merge_mask import merge_mask
 
 ITERATION = 3
 SIZE = 512
+BLACK = (0, 0, 0)
 
 if __name__ == "__main__":
-    images = os.listdir("./test_sets/images")
-    color_masks = os.listdir("./test_sets/color_masks")
+    image_path = "./test_sets/images"
+    color_mask_path = "./test_sets/color_masks"
+    images = os.listdir(image_path)
+    color_masks = os.listdir(color_mask_path)
 
-    base_mask = np.ones((SIZE, SIZE))
+    base_mask = [BLACK for _ in range(SIZE * SIZE)]
 
-    # for _ in range(ITERATION):
-    for image in images:
-        # generate masks
-        segmentation.imgSeg(image)
+    for _ in range(ITERATION):
+        for image in images:
+            # generate masks
+            segmentation.imgSeg(image)
+            base_mask = merge_mask(f"{color_mask_path}/{image}_mask.png", base_mask)
     inpainting.inpaint()
 
