a59ed0ef8debce4c923aea4122e54e6876969207
lbwei <0800680274united@gmail.com>
2023-06-06 12:03:42 +0800

    size 512 solved

--- inpainting.py
+++ inpainting.py
@@ -4,7 +4,8 @@ network = "./pretrained/CelebA-HQ_512.pkl"
 dpath = "./test_sets/images"
 mpath = "./test_sets/masks"
 outdir = "./test_sets/images"
+resolution = 1024
 
 def inpaint():
-    generate_images(network_pkl=network, dpath=dpath, mpath=mpath, outdir=outdir, noise_mode='random')
+    generate_images(network_pkl=network, dpath=dpath, mpath=mpath, outdir=outdir, noise_mode='random', resolution=resolution)
 
